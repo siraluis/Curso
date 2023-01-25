3dd4c18170ef8c2109e823f5ad277d55e9c23b7f
Haonter <101908524+Haonter@users.noreply.github.com>
2023-01-25 02:12:15 -0300

    JavaScript

--- JavaScript/Seccion_1/Operadores_de_Comparacion.js
+++ JavaScript/Seccion_1/Operadores_de_Comparacion.js
@@ -3,34 +3,15 @@
     realizar operaciones binarias(entre 2 elementos) de comparacion.
 */
 
-/*
-    Ejemplo:
-*/
-// Asignacion = : como bien su nombre indica se utiliza para asignar un valor.
-
-let asignacion = "Diego";
-let numero1 = 1;
-let numero2 = 2;
-let caracter1 = "1";
-//Array
-let array = [1,2,3,4,5];
-//Objeto
-let otroNumero1 = {
-    nombre: "Diego", 
-    edad: 30, 
-    pais: "Alemania"
-};
-
-
 
 // Igual que == : Devuelve true si los elementos son iguales 
 // (No toma en cuenta el tipo de dato, solo el valor).
 
 //true
-console.log(numero1 == caracter1);
+console.log(1 == "1");
 
 //false
-console.log(numero1 == numero2);
+console.log(1 == 2);
 
 
 
@@ -51,10 +32,10 @@ let caracter_1 = "1";
 let numero_1 = 1;
 
 
-console.log( numero === caracter_1) ; 
-console.log( numero === numero_2) ; 
-console.log( numero === 1) ; 
-console.log( numero === numero_1) ; 
+console.log( 1 === "1") ; 
+console.log( 1 === 1) ; 
+console.log( 1 === 2) ; 
+console.log( 1 === "2") ; 
 
 
 
@@ -70,26 +51,22 @@ console.log( 1 !== "2");
 
 // Mayor que > : Devuelve true si el operando izquierdo es mayor que el operando derecho.
 
-let uno = 1;
-let dos = 2;
-let UNO = 1;
-
-console.log(dos > uno);
-console.log(uno > dos);
+console.log(2 > 1);
+console.log(1 > 2);
 
 
 // Menor que < : Devuelve true si el operando izquierdo es menor que el operando derecho.
-console.log(uno < dos);
-console.log(dos < uno);
+console.log(2 < 1);
+console.log(1 < 2);
 
 
 //  Mayor o igual que >= : 	Devuelve true si el operando izquierdo es mayor o igual que el operando derecho.
-console.log(uno >= UNO);
-console.log(dos >= dos);
-console.log(dos >= uno );
+console.log(2 >= 1 );
+console.log(1 >= 2);
+console.log(1 >= 1);
 
 
 //  Menor o igual que <= : Devuelve true si el operando izquierdo es menor o igual que el operando derecho.
-console.log(uno <= dos);
-console.log(uno <= UNO);
-console.log(uno <= 0);
+console.log(2 <= 1);
+console.log(1 <= 2);
+console.log(1 <= 1);
