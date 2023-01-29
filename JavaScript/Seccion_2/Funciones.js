c93dac51eb8809ea69a1cb5a36116a81745f4971
Haonter <101908524+Haonter@users.noreply.github.com>
2023-01-29 15:30:17 -0300

    JavaScript - Funciones
    Solucion de Primer ejercicio añadida.

--- JavaScript/Seccion_2/Funciones.js
+++ JavaScript/Seccion_2/Funciones.js
@@ -29,42 +29,52 @@ console.log( mifuncion2(100) );
 //Funcion para generar un mensaje predeterminado con una parte personalizada
 
 function saludar(nombre){
-    return (`Hola, me llamo ${nombre}, y estoy aprendiendo JavaScript `);
+    return (`Hola, me llamo ${nombre}, y estoy aprendiendo JavaScript!`);
 }
 
 console.log(saludar("Luzvelia"));
 
 
-//Ejemplo 2
-let fondos = 1000;
-function retiroDeDinero(cantidad){
-    fondos = fondos - cantidad; 
-    return `Retiraste ${cantidad}, tu saldo restante es ${fondos}`;
-}
-
-console.log(retiroDeDinero(230));
-console.log(retiroDeDinero(100));
-console.log(retiroDeDinero(270));
-console.log(retiroDeDinero(60));
-
-
 
 //Practica 1:
-// En una funcion, elabora la logica de un cajero automatico que retorne un mensaje de fondo insuficiente
-// cuanto el saldo sea menor o igual a 0
+// En una funcion, elabora la logica de un cajero automatico que retorne el mensaje: "Saldo insuficiente"
+// cuanto el saldo sea menor o igual a 0;
+
+let saldo = 1000;
+function retiroDeDinero(cantidad){
+    if((saldo - cantidad) <= 0){
+        return `Saldo insuficiente, tu saldo es ${saldo}$`;
+    }else if (saldo - cantidad > 0){
+        saldo-= cantidad; 
+        return `Retiraste ${cantidad}$, saldo restante: ${saldo}$`;
+    }
+}
+
+console.log(retiroDeDinero(100));
+console.log(retiroDeDinero(100));
+console.log(retiroDeDinero(600));
+console.log(retiroDeDinero(201));
+
 
 // practica 2:
 // Elabora una funcion que compare 2 numeros y retorne si es par o impar
 // tip: usar %
 
+
+
+
 // practica 3:
 // Elabora una funcion sin parametros que retorne si la multiplicacion de 3 x 1260
 // es par o impar.
 
+
+
+
+
 // practica 4:
 // Crea una funcion que reciba los parametros:
-// nombre1, nombre2, apellido, edad, sexo, pais ingresados mediante propmt,
+// nombre1, nombre2, apellido, edad, sexo, pais ingresados mediante prompt,
 // los compare y retorne si son identicos.
 // en caso de ser identicos, solicitar un apellido y mostrar el mensaje:
-// Hola, Me llamo (nombre)(apellido), soy (sexo), tengo (edad) años, y vivo en (pais).
+// Hola, Me llamo (nombre) (apellido), soy (sexo), tengo (edad) años, y vivo en (pais).
 // Si los nombres no coinciden, retornar, Los nombres deben ser identicos, intentalo nuevamente.
